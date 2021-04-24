ac9b16f9d345a1b44477fdc735743b248ad1c5c0
Rani Saad <rsaad@live.ca>
2021-04-23 20:51:41 -0400

    Added more readme content

--- Readme.md
+++ Readme.md
@@ -1,5 +1,15 @@
-# This is a readme test
 
-Project Name: Poprprp
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)  
+# This is a readme test 
+## Table of Contents:
+Project Name: Zoom 
+Project Description: Test
+Installation Guide: Test
+Usage: Test
+Contribution Guidelines: Test
+Test Methods: Test 
+License: ISC,MIT,BSD,Apache 2.0,GNU General Public License V3.0
+Github Username: Test
+Email: Test
 
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  
--- src/readmeTemplate.js
+++ src/readmeTemplate.js
@@ -1,7 +1,17 @@
 const generateReadme = readmeData => {
   return `
 # This is a readme test 
-Project Name: ${readmeData.project}  
+## Table of Contents:
+Project Name: ${readmeData.project} 
+Project Description: ${readmeData.description}
+Installation Guide: ${readmeData.installation}
+Usage: ${readmeData.usage}
+Contribution Guidelines: ${readmeData.contribution}
+Test Methods: ${readmeData.test} 
+License: ${readmeData.license}
+Github Username: ${readmeData.github}
+Email: ${readmeData.email}
+
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
   `;
 };
