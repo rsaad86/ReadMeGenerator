1325ef4ecb76ca3d301145bc113d40dccde92d55
Rani Saad <rsaad@live.ca>
2021-04-23 20:54:03 -0400

    Added to readme file

--- Readme.md
+++ Readme.md
@@ -1,15 +1,15 @@
 
 # This is a readme test 
 ## Table of Contents:
-Project Name: Zoom 
-Project Description: Test
-Installation Guide: Test
-Usage: Test
-Contribution Guidelines: Test
-Test Methods: Test 
-License: ISC,MIT,BSD,Apache 2.0,GNU General Public License V3.0
-Github Username: Test
-Email: Test
+Project Name: Test Pro, 
+Project Description: ok,
+Installation Guide: ok,
+Usage: ok,
+Contribution Guidelines: ok,
+Test Methods: ok,
+License: ISC,MIT,BSD,Apache 2.0,GNU General Public License V3.0,
+Github Username: ok,
+Email: ok
 
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
   
--- src/readmeTemplate.js
+++ src/readmeTemplate.js
@@ -2,14 +2,14 @@ const generateReadme = readmeData => {
   return `
 # This is a readme test 
 ## Table of Contents:
-Project Name: ${readmeData.project} 
-Project Description: ${readmeData.description}
-Installation Guide: ${readmeData.installation}
-Usage: ${readmeData.usage}
-Contribution Guidelines: ${readmeData.contribution}
-Test Methods: ${readmeData.test} 
-License: ${readmeData.license}
-Github Username: ${readmeData.github}
+Project Name: ${readmeData.project}, 
+Project Description: ${readmeData.description},
+Installation Guide: ${readmeData.installation},
+Usage: ${readmeData.usage},
+Contribution Guidelines: ${readmeData.contribution},
+Test Methods: ${readmeData.test},
+License: ${readmeData.license},
+Github Username: ${readmeData.github},
 Email: ${readmeData.email}
 
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
